f568e2b0b2f90c6ddccfc5b413426a8dddc7c4d4
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-08-10 14:17:33 +0800

    SlideImage now render dynamically through props

--- src/components/SlideImage.js
+++ src/components/SlideImage.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import laptop from '../assets/images/laptop.jpg';
 
-export default function SlideImage() {
+export default function SlideImage({ image, title, active }) {
+  let classes = 'w-full border-gray border-2';
+  if (active === false) {
+    classes = classes + ' hidden';
+  }
   return (
-    <div className="w-full border-gray border-2">
-      <img className="w-full" src={laptop} alt={laptop}></img>
+    <div className={classes}>
+      <img className="w-full" src={image} alt={title}></img>
       <h2 className="text-gray-700 font-semibold bg-gray-300 text-center text-4xl object-top">
-        Title
+        {title}
       </h2>
     </div>
   );
