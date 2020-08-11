55cab6697a12340677e29955728a4f3f5a24b499
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-08-11 15:16:10 +0800

    create register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppHeader from './components/AppHeader';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
           </Route>
         </Switch>
       </div>
---
+++ src/pages/Register.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+export default function Login() {
+  const [username, setusername] = useState('');
+  const [password, setpassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
+
+  return (
+    <div className="bg-gray-200 min-h-login mx-auto pt-8">
+      <form className="bg-white max-w-md mx-auto shadow-lg rounded px-8 py-6 mb-4">
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-base font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full px-3 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            name="password"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setusername(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-base font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          ></input>
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-base font-bold mb-2"
+            htmlFor="confirmpassword"
+          >
+            Confirm Password
+          </label>
+          <input
+            id="confirmpassword"
+            placeholder="Confirm Password"
+            type="password"
+            name="confirmpassword"
+            value={confirmPassword}
+            onChange={(e) => {
+              setconfirmPassword(e.target.value);
+            }}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          ></input>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
