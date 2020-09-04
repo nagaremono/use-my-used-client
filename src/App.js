a8076194a2bbc8d1e8e8cca137e522f3a1fcf728
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-09-04 12:32:56 +0800

    add global provider to the app

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { GlobalProvider } from './context/GlobalContext';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppHeader />
-        <Switch>
-          <Route path="/" exact>
-            <Landing />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <GlobalProvider>
+      <Router>
+        <div className="App">
+          <AppHeader />
+          <Switch>
+            <Route path="/" exact>
+              <Landing />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </GlobalProvider>
   );
 }
 
